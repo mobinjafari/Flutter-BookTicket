e7c5ca224fac71f1f3bdd9d06ba33ce5d8625f23
Mobin jafari <support@lotka.org>
2022-12-15 14:15:01 +0330

    bottom navigation completely done

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,69 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+
+    const Text("home"),
+    const Text("search"),
+    const Text("tickets"),
+    const Text("profile")
+  ];
+
+  void _onItemTapped(int index) {
+    setState((){
+      selectedIndex = index;
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _widgetOptions[selectedIndex]),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: _onItemTapped ,
+        type: BottomNavigationBarType.fixed,
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xFF526480),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled) ,
+              label: "home"),
+
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: "search"),
+
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: "airplane_ticket"),
+
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "person")
+
+        ],
+      ),
+    );
+  }
+
+
+}
